1bf49021d57ed846cf1f6752e69471cb7ed12216
mikerogers04 <mikerogers04@hotmail.com>
2023-04-12 16:55:32 -0700

    Removed uncommented code from App.js and backend controller.

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,6 +1,2 @@
 class ApartmentsController < ApplicationController
-    def index
-        apartments = Apartment.all
-        render json: apartments
-    end
 end
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import ApartmentIndex from "./pages/ApartmentIndex";
-// import ApartmentShow from "./pages/ApartmentShow"
-// import ApartmentNew from "./pages/ApartmentNew"
-// import ApartmentEdit from "./pages/ApartmentEdit"
-// import ProtectedIndex from "./pages/ProtectedIndex";
 import Home from "./pages/Home";
-// import NotFound from "./pages/NotFound"
 
 const App = (props) => {
   return (
