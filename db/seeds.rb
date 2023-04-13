bc8d9677488f45695fc71eb4282e58617928f371
BxRay95 <bray387@outlook.com>
2023-04-13 11:54:33 -0700

    added index code, seed not working, swapping drivers to test others capabilities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,43 @@
-# user1 = User.where(email: 'Worldwideapartments@gmail.com').first_or_create(password: 'iamthegreatest123', password_confirmation: 'iamthegreatest123')
-user2 = User.where(email: 'hallwaytest@testing.com').first_or_create(password: 'hallwaysboyztest', password_confirmation: 'hallwaysboyztest')
+user1 = User.where(email: 'hallwaytest@testing.com').first_or_create(password:'hallwaysboyztest', password_confirmation: 'hallwaysboyztest')
 
-#     user1_apartments = [
-#     {
-#         bedrooms: 5,
-#         bathrooms: 1.5,
-#         address: "123 fourth street",
-#         city: "Miami",
-#         state: "FL",
-#         square_footage: 600, 
-#         price: 300000, 
-#         utilities: "washer, dryer, gas and electric",
-#         pets: true, 
-#         image: "http://image.com"
-#     }
-# ]
-    
-    user2_apartments = [
+    user1_apartments = [
     {
         bedrooms: 2,
         bathrooms: 1.5,
-        address: "42 southest 9th",
-        city: "Los Angeles",
-        state: "CA",
+        address: '42 southest 9th',
+        city: 'Los Angeles',
+        state: 'CA',
         square_footage: 900,
         price: 900000,
-        utilities: "Not included",
+        utilities: 'Not included',
         pets: false,
-        image: "http://image.com"
+        image: 'http://image.com'
     }
 ]
-    # user1_apartments.each do |apartment|
-    #     user1.apartments.create(apartment)
-    #     p "created: #{apartment}"
-    # end
     
-    user2_apartments.each do |apartment|
-        user2.apartments.create(apartment)
+    user1_apartments.each do |apartment|
+        user1.apartments.create(apartment)
         p "created: #{apartment}"
     end 
+
+user2 = User.where(email: 'hallwaytesting@testing.com').first_or_create(password:'hallwaysboyztesting', password_confirmation: 'hallwaysboyztesting')
+
+user2_apartments = [
+    {
+        bedrooms: 2,
+        bathrooms: 1.5,
+        address: '42 southest 9th',
+        city: 'Los Angeles',
+        state: 'CA',
+        square_footage: 900,
+        price: 900000,
+        utilities: 'Not included',
+        pets: false,
+        image: 'http://image.com'
+    }
+]
+
+user2_apartments.each do |apartment|
+    user2.apartments.create(apartment)
+    p "created: #{apartment}"
+end
--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
+  let (:user) {User.create email: 'hallwaytesting@testing.com', password: 'hallwaysboyztesting', password_confirmation: 'hallwaysboyztesting'}
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of apartments" do 
+      user.apartments.create(
+      bedrooms: 2,
+      bathrooms: 1.5,
+      address: "42 southest 9th",
+      city: "Los Angeles",
+      state: "CA",
+      square_footage: 900,
+      price: 900000,
+      utilities: "Not included",
+      pets: false,
+      image: "http://image.com")
+
+      get '/apartments'
+      apartment = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(apartment.length).to eq 1
+    end
   end
 end
