1d1ec2cf5cd39d44b35b3e411896e017e340cbb9
mikerogers04 <mikerogers04@hotmail.com>
2023-04-13 13:29:24 -0700

    Adds index controller method and testing for index.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-# user1 = User.where(email: 'Worldwideapartments@gmail.com').first_or_create(password: 'iamthegreatest123', password_confirmation: 'iamthegreatest123')
+user1 = User.where(email: 'Worldwideapartments@gmail.com').first_or_create(password: 'iamthegreatest123', password_confirmation: 'iamthegreatest123')
 user2 = User.where(email: 'hallwaytest@testing.com').first_or_create(password: 'hallwaysboyztest', password_confirmation: 'hallwaysboyztest')
 
-#     user1_apartments = [
-#     {
-#         bedrooms: 5,
-#         bathrooms: 1.5,
-#         address: "123 fourth street",
-#         city: "Miami",
-#         state: "FL",
-#         square_footage: 600, 
-#         price: 300000, 
-#         utilities: "washer, dryer, gas and electric",
-#         pets: true, 
-#         image: "http://image.com"
-#     }
-# ]
+    user1_apartments = [
+    {
+        bedrooms: 5,
+        bathrooms: 1.5,
+        address: "123 fourth street",
+        city: "Miami",
+        state: "FL",
+        square_footage: 600, 
+        price: 300000, 
+        utilities: "washer, dryer, gas and electric",
+        pets: true, 
+        image: "http://image.com"
+    }
+]
     
     user2_apartments = [
     {
@@ -30,12 +30,12 @@ user2 = User.where(email: 'hallwaytest@testing.com').first_or_create(password: '
         image: "http://image.com"
     }
 ]
-    # user1_apartments.each do |apartment|
-    #     user1.apartments.create(apartment)
-    #     p "created: #{apartment}"
-    # end
+user1_apartments.each do |apartment|
+user1.apartments.create(apartment)
+p "created: #{apartment}"
+end
     
-    user2_apartments.each do |apartment|
-        user2.apartments.create(apartment)
-        p "created: #{apartment}"
-    end 
+user2_apartments.each do |apartment|
+    user2.apartments.create(apartment)
+    p "created: #{apartment}"
+end 
